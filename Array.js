8242802894c994d3f79fca3ccbae604d1a9f3aa3
Mareco Nabors <naborsmarico99@gmail.com>
2018-08-01 21:37:54 -0500

    Array/FizzBuzz

---
+++ Array.js
@@ -0,0 +1,14 @@
+var myNumberList = [12, 2112, 9000, 1999, 24];
+
+console.log(myNumberList);
+
+for (x=0;x<myNumberList;x++){
+    console.log(myNumberList)
+
+}
+var largestNum=myNumberList[2];
+console.log(myNumberList[2]);
+
+
+console.log(myNumberList[Math.max(1)]);
+
---
+++ FizzBuzz.js
@@ -0,0 +1,22 @@
+function fizzBuzz() {
+
+}
+for (var x =1; x<=100; x++){
+
+    if (x % 3 == 0 && x % 5==0){
+        console.log('FizzBuzz');
+    }
+    else if (x % 3 == 0){
+        console.log('Fizz');
+    }
+    else if (x % 5 == 0){
+
+
+    }
+  else {
+        console.log(x)
+
+    }
+  }
+
+fizzBuzz();
