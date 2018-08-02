48e8b4e11ee18d8e17e3a69bf3745b1d6c7eb64a
Kevin-CodeCrew <kevin@code-crew.org>
2018-08-02 01:34:36 -0500

    Graded 08-02-2018 01:32:33

--- Array.js
+++ Array.js
@@ -1,14 +1,23 @@
 var myNumberList = [12, 2112, 9000, 1999, 24];
 
-console.log(myNumberList);
+// console.log(myNumberList);
 
-for (x=0;x<myNumberList;x++){
-    console.log(myNumberList)
+// for (x=0;x<myNumberList;x++){
+//     console.log(myNumberList)
 
+// }
+// var largestNum=myNumberList[2];
+// console.log(myNumberList[2]);
+
+
+console.log(getMaxOfArray(myNumberList));
+
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
 }
-var largestNum=myNumberList[2];
-console.log(myNumberList[2]);
 
+/*
+	KEY: Wow! You were *really* close to finding an alternate approach by using Math.max. Good job!
 
-console.log(myNumberList[Math.max(1)]);
-
+	Other than that though, your code was broken :-P
+*/
--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,6 @@
 function fizzBuzz() {
 
-}
+} // This function is empty so no point having it
 for (var x =1; x<=100; x++){
 
     if (x % 3 == 0 && x % 5==0){
@@ -20,3 +20,16 @@ for (var x =1; x<=100; x++){
   }
 
 fizzBuzz();
+
+/*
+
+    KEY: Works! Excellent Job!
+
+    
+    Didn't prompt for end value though...
+    
+    The fizzBuzz() function was unecessary as it has an empty body.
+
+    The code still works though from outside of the body.
+
+*/
